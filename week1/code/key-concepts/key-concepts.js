632fe25276386bfbe6353b22989303b540641042
Rimmert Zelle <ra_zelle@hotmail.com>
2014-05-09 07:58:04 +0200

    added default code week 1
    added default code week 1

--- week1/code/key-concepts/key-concepts.js
+++ week1/code/key-concepts/key-concepts.js
@@ -10,63 +10,16 @@
 */
 
 var iValue = 20; // primitive value
-var iCurrentValue = iValue;
-
-console.log('iValue: '+iValue);
-console.log('iCurrentValue: '+iCurrentValue);
-
-iValue = iValue+20;
-console.log('iValue added with 20:');
-console.log('iValue: '+iValue);
-console.log('iCurrentValue: '+iCurrentValue);
-
 var oAccount = {amount:40}; //referenced value
-var oNewAccount = oAccount;
-
-console.log('oAccount: '+oAccount.amount);
-console.log('oNewAccount: '+oNewAccount.amount);
-
-oAccount.amount = oAccount.amount+50;
-console.log('oAccount added with 30:');
-console.log('oAccount: '+oAccount.amount);
-console.log('oNewAccount: '+oNewAccount.amount);
-
 
 /*
 *   Assignment: amount function with multiple parameters
 */
 
-// function amount() {
-//   var result = 0,
-//       i = 0,
-//       len = arguments.length;
-//   while (i < len) {
-//     result += arguments[i];
-//     i++;
-//   }
-//   return result;
-// }
-
-// console.log(amount(1, 2)); // 3
-// console.log(amount(3, 4, 5, 6)); // 18
-// console.log(amount(50)); // 50
-// console.log(amount()); // 0
+function amount() {}
 
 /*
 *   Assignment: simple functional programming
 */
 
-// function alertArray(array) {
-//   for (var i = 0; i < array.length; i++)
-//     alert(array[i]);
-// }
-
-// alertArray([1,2,3,4]);
-
-// function forEach(array, action) {
-//   for (var i = 0; i < array.length; i++){
-//     action(array[i]);
-//   }
-// }
-
-// forEach(["Wampeter", "Foma", "Granfalloon"], alert);
+function alertArray(array) {}
